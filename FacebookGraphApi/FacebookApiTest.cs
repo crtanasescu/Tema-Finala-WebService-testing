2c2382a17600ffa4b4c68adbe580d0ce5b68405e
Cristina Tanasescu <crtanasescu@endava.com>
2016-08-09 19:21:16 +0300

    WebService test

---
+++ FacebookGraphApi/FacebookApiTest.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacebookGraphApi
+{
+    [TestFixture]
+    public class FacebookApiTest
+    {
+
+        private FacebookGraphApi _fbGraphApi;
+
+        [SetUp]
+        public void SetUp()
+        {
+            //parametrul trebuie schimbat cu un access token generat cu https://developers.facebook.com/tools/explorer
+            _fbGraphApi = new FacebookGraphApi("EAACEdEose0cBAPI5BjSgI1psFbZBTHi9oEWGZBKb38KuI77k4kNR3ZCztXBZCKd1Nk6vKcSeEan7SrewxXDCaMnXMdQ6oalwKdzPZCOkdNn8QL9ZAwE2KVijVPsKqZAmiREaLgXwEuZBFPd04v6gwpad9pewydlwIt32qyF0HGlAURR1tomeZBS0t");
+
+        }
+
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+
+        [Test]
+        public void TestApi()
+        {
+
+          
+            
+            Trace.WriteLine("1.Detalii personale---------------------");
+            Trace.WriteLine(_fbGraphApi.GetMyDetails());
+            var test = _fbGraphApi.GetMyDetails();
+            Assert.IsTrue(_fbGraphApi.GetMyDetails().Contains("Tanasescu"));
+          
+
+            Trace.WriteLine("2.Prieteni---------------------");
+            Trace.WriteLine(_fbGraphApi.GetFriends());
+
+
+            //verificare prietenie dupa un id primit in functia anterioara
+            Trace.WriteLine("3.Verificare prietenie---------------------");
+            Trace.WriteLine(_fbGraphApi.CheckFriendship("523450737766772"));
+            Assert.IsTrue(_fbGraphApi.GetFriends().Contains("\"total_count\":2"));
+
+
+            Trace.WriteLine("4.Pozele mele---------------------");
+            Trace.WriteLine(_fbGraphApi.GetPhotos());
+
+
+            Trace.WriteLine("5.Adaugare comentariu la una din pozele de mai sus---------------------");
+            _fbGraphApi.CommentPhoto("119644731813680", "test");
+
+
+
+            Trace.WriteLine("6.News Feed-uri ---------------------------");
+            Trace.WriteLine(_fbGraphApi.GetFeed());
+            Assert.IsTrue(_fbGraphApi.GetFeed().Contains("Facebook is cool!"));
+
+
+
+
+            // Trace.WriteLine(fbGraphApi.GetFriends());
+
+        }
+    }
+}
---
+++ FacebookGraphApi/FacebookGraphApi.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacebookGraphApi
+{
+    public class FacebookGraphApi
+    {
+        private HttpClient _httpClient;
+        private string _baseUrl = "https://graph.facebook.com/v2.7";
+
+        //accesToken
+        public FacebookGraphApi(string access_token)
+        {
+            _httpClient = new HttpClient();
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("OAuth",access_token);
+
+        }
+
+        //intoarce detaliile userului: nume si id
+        public string GetMyDetails()
+        {
+            return Get(_baseUrl + "/me");
+        }
+
+       //verifica daca exista prietenie intre contul curent si contul userului cu id-ul dat
+        public string CheckFriendship(string possibleFriendId)
+        {
+            if (string.IsNullOrEmpty(possibleFriendId))
+                return "";
+            return Get(_baseUrl + "/me/friends/"+ possibleFriendId);
+        }
+
+        //returneaza toti prietenii
+        public string GetFriends()
+        {
+            return Get(_baseUrl + "/me/friends");
+        }
+
+        //returneaza lista cu toate pozele uploadate
+        public string GetPhotos()
+        {
+            return Get(_baseUrl + "/me/photos/uploaded");
+        }
+
+        //comenteaza poza cu id-ul dat
+        public void CommentPhoto(string photoId, string message)
+        {
+            var objectToPost = new List<KeyValuePair<string, string>>();
+            objectToPost.Add(new KeyValuePair<string, string>("message", message));
+            Post(_baseUrl + "/" + photoId + "/comments", objectToPost);
+        }
+
+        //returneaza newsfeed-urile
+        public string GetFeed()
+        {
+            return Get(_baseUrl + "/me/feed");
+        }
+
+
+        //GET
+        private string Get(string url)
+        {
+            return GetJsonResult(_httpClient.GetAsync(url).Result);
+        }
+
+        //POST
+        private void Post(string url,IEnumerable<KeyValuePair<string,string>> postObject)
+        {
+            var result = _httpClient.PostAsync(url, new FormUrlEncodedContent(postObject)).Result;
+            var te = GetJsonResult(result);
+        }
+
+        private string GetJsonResult(HttpResponseMessage message)
+        {
+            return message.Content.ReadAsStringAsync().Result;
+        }
+    }
+}
